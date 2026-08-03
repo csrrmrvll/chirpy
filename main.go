f0ca01f0a97028279e1cc7433d1bd1fa3348feb3
César Romero Avello <cesar.romero.avello@ing.com>
2026-08-03 14:17:50 +0200

    feat(http): default http server implemented

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	serveMux := http.NewServeMux()
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        serveMux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
 }
