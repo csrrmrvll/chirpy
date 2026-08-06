e318e1170f773215e2b486ea507df5063fb76bdd
César Romero Avello <cesar.romero.avello@ing.com>
2026-08-06 09:36:52 +0200

    feat(http): improved using bootdotdev solution files

--- metrics.go
+++ metrics.go
@@ -8,12 +8,12 @@ import (
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write(fmt.Appendf(nil, "<html>"+
-		"<body>"+
-		"<h1>Welcome, Chirpy Admin</h1>"+
-		"<p>Chirpy has been visited %d times!</p>"+
-		"</body>"+
-		"</html>", cfg.fileserverHits.Load()))
+	w.Write(fmt.Appendf(nil, `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load()))
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
