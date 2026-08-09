e435dfed33f6a34d91db1a0d56b52dcef7d575a2
César Romero Avello <cesar.romero.avello@ing.com>
2026-08-09 11:06:37 +0200

    feat(api): improved using bootdotdev solution files

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -6,6 +6,29 @@ import (
 	"github.com/google/uuid"
 )
 
+func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
+	chirpIDString := r.PathValue("chirpID")
+	chirpID, err := uuid.Parse(chirpIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid chirp ID", err)
+		return
+	}
+
+	dbChirp, err := cfg.db.GetChirp(r.Context(), chirpID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't get chirp", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, Chirp{
+		ID:        dbChirp.ID,
+		CreatedAt: dbChirp.CreatedAt,
+		UpdatedAt: dbChirp.UpdatedAt,
+		UserID:    dbChirp.UserID,
+		Body:      dbChirp.Body,
+	})
+}
+
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
 	dbChirps, err := cfg.db.GetChirps(r.Context())
 	if err != nil {
@@ -25,27 +48,4 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 	}
 
 	respondWithJSON(w, http.StatusOK, chirps)
-}
-
-func (cfg *apiConfig) handlerChirpRetrieve(w http.ResponseWriter, r *http.Request) {
-	chirpIDStr := r.URL.Path[len("/api/chirps/"):]
-	chirpID, err := uuid.Parse(chirpIDStr)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid chirp ID", err)
-		return
-	}
-
-	chirp, err := cfg.db.GetChirpByID(r.Context(), chirpID)
-	if err != nil {
-		respondWithError(w, http.StatusNotFound, "Chirp not found", err)
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, Chirp{
-		ID:        chirp.ID,
-		CreatedAt: chirp.CreatedAt,
-		UpdatedAt: chirp.UpdatedAt,
-		UserID:    chirp.UserID,
-		Body:      chirp.Body,
-	})
 }
