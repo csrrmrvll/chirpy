8844aa22d11b4a15eabc59780af4fb8af4c974c4
César Romero Avello <cesar.romero.avello@ing.com>
2026-08-14 09:05:39 +0200

    feat(api): sorting by query param

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"sort"
 
 	"github.com/csrrmrvll/chirpy/internal/database"
 	"github.com/google/uuid"
@@ -42,6 +43,15 @@ func authorIDFromRequest(r *http.Request) (uuid.UUID, error) {
 	return authorID, nil
 }
 
+func sortingFromRequest(r *http.Request) string {
+	sort := r.URL.Query().Get("sort")
+	switch sort {
+	case "asc", "desc":
+		return sort
+	}
+	return "asc"
+}
+
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
 	authorID, err := authorIDFromRequest(r)
 	if err != nil {
@@ -72,5 +82,12 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 		})
 	}
 
+	sortDesc := sortingFromRequest(r)
+	sort.Slice(chirps, func(i, j int) bool {
+		if sortDesc == "asc" {
+			return chirps[i].CreatedAt.Before(chirps[j].CreatedAt)
+		}
+		return chirps[i].CreatedAt.After(chirps[j].CreatedAt)
+	})
 	respondWithJSON(w, http.StatusOK, chirps)
 }
